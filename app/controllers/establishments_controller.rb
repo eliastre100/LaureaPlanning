02c90d1155d15b39d48d57e54f91a2ff446737bd
Antoine FORET <antoine.foret@epitech.eu>
2018-05-30 12:46:48 +0200

    [~] Updated Establishment (built manually)

--- app/controllers/establishments_controller.rb
+++ app/controllers/establishments_controller.rb
@@ -1,74 +1,50 @@
 class EstablishmentsController < ApplicationController
   before_action :set_establishment, only: [:show, :edit, :update, :destroy]
 
-  # GET /establishments
-  # GET /establishments.json
   def index
     @establishments = Establishment.all
   end
 
-  # GET /establishments/1
-  # GET /establishments/1.json
-  def show
-  end
-
-  # GET /establishments/new
   def new
     @establishment = Establishment.new
   end
 
-  # GET /establishments/1/edit
+  def show
+  end
+
   def edit
   end
 
-  # POST /establishments
-  # POST /establishments.json
   def create
     @establishment = Establishment.new(establishment_params)
 
-    respond_to do |format|
-      if @establishment.save
-        format.html { redirect_to @establishment, notice: 'Establishment was successfully created.' }
-        format.json { render :show, status: :created, location: @establishment }
-      else
-        format.html { render :new }
-        format.json { render json: @establishment.errors, status: :unprocessable_entity }
-      end
+    if @establishment.save
+      redirect_to @establishment, notice: 'Establishment was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /establishments/1
-  # PATCH/PUT /establishments/1.json
   def update
-    respond_to do |format|
-      if @establishment.update(establishment_params)
-        format.html { redirect_to @establishment, notice: 'Establishment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @establishment }
-      else
-        format.html { render :edit }
-        format.json { render json: @establishment.errors, status: :unprocessable_entity }
-      end
+    if @establishment.update(establishment_params)
+      redirect_to @establishment, notice: 'Establishment was successfully updated.'
+    else
+      render :new
     end
   end
 
-  # DELETE /establishments/1
-  # DELETE /establishments/1.json
   def destroy
     @establishment.destroy
-    respond_to do |format|
-      format.html { redirect_to establishments_url, notice: 'Establishment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to establishments_url
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_establishment
-      @establishment = Establishment.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def establishment_params
-      params.require(:establishment).permit(:name)
-    end
+  def set_establishment
+    @establishment = Establishment.find(params[:id])
+  end
+
+  def establishment_params
+    params.require(:establishment).permit(:name)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_28_120204) do
+ActiveRecord::Schema.define(version: 2018_05_30_065511) do
 
   create_table "establishments", force: :cascade do |t|
     t.string "name"
