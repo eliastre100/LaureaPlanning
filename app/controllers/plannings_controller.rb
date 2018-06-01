7d5393ff3bb87f4e912e1455d25ecbed05df1afc
Antoine FORET <antoine.foret@epitech.eu>
2018-06-01 22:48:41 +0200

    [+] Added conflict detection

--- app/controllers/plannings_controller.rb
+++ app/controllers/plannings_controller.rb
@@ -1,16 +1,17 @@
 class PlanningsController < ApplicationController
   def index
     @instances = instances
+    @conflicts = conflicts
   end
 
   def create
-    instance = Instance.find(params[:id])
+    instance            = Instance.find(params[:id])
     session[:instances] = instances_ids.push(instance.id) unless instances_ids.include?(instance.id)
     redirect_back fallback_location: root_path, notice: "Successfully picked instance #{instance.code}"
   end
 
   def destroy
-    instance = Instance.find(params[:id])
+    instance            = Instance.find(params[:id])
     session[:instances] = instances_ids - [instance.id]
     redirect_back fallback_location: root_path, notice: "Successfully unpicked instance #{instance.code}"
   end
@@ -22,8 +23,17 @@ class PlanningsController < ApplicationController
   end
 
   def instances
-    instances_ids.map do |id|
+    @instance = @instances || instances_ids.map do |id|
       Instance.find(id)
+    end
+  end
+
+  def conflicts
+    lessons = instances.map(&:lessons).flatten
+    lessons.select do |lesson|
+      lessons.any? do |other|
+        lesson != other && lesson.start_at <= other.end_at && lesson.end_at >= other.start_at
+      end
     end
   end
 end
